8d23f7ed3cdbc9c72f01bda68e0e5a8a4c7888a5
William <110365948+contrarybaton60@users.noreply.github.com>
2025-05-25 16:56:01 -0400

    Update appman.sh

--- scripts/appman.sh
+++ scripts/appman.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
+echo 'alias am="appman $@"' >> .bashrc
 # Generic bash file for AM/AppMan
 wget -q https://raw.githubusercontent.com/ivan-hc/AM/main/AM-INSTALLER
 chmod a+x ./AM-INSTALLER
 ./AM-INSTALLER
 
-# other commands I use for personal stuff
-## am extra zyachel/luanti-appimage luanti
-## am extra bmsuseluda/ares-emu-appimage ares
-## am install vscodium lutris librewolf steam obsidian prismlauncher audacity gimp stellarium wine-stable
-## am download supertux supertuxkart woof-doom retroarch
-## am icons --all
+
+am install retroarch lutris stellarium libreoffice steam wine kdenlive gimp vscodium vlc audacity obsidian vesktop librewolf firefox prismlauncher yt-dlp ventoy easytag ntsc-rs appimageupdatetoll archimage-cli
+am download supertux supertuxkart woof-doom
+am icons --all
 
 # extend functionality with Junest, but why not just use distrobox?
 ## am install junest
-
-##echo 'alias am="appman $@"' >> .bashrc
