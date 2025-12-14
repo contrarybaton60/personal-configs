ac9425074c12d02bb677920a6c4c9b3a5b898229
contrarybaton60 <110365948+contrarybaton60@users.noreply.github.com>
2025-12-13 22:51:08 -0500

    Delete scripts directory

--- scripts/adoptium.sh
+++
@@ -1,7 +0,0 @@
-# This is a script for installing Adoptium OpenJDK on Debian-based GNU/Linux, make sure that your release codename is correctly specified. 
-
-
-wget -qO - https://packages.adoptium.net/artifactory/api/gpg/key/public | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/adoptium.gpg > /dev/null
-echo "deb https://packages.adoptium.net/artifactory/deb $(awk -F= '/bookworm/{print$2}' /etc/os-release) main" | sudo tee /etc/apt/sources.list.d/adoptium.list
-sudo apt update
-sudo apt install temurin-8-jdk temurin-17-jdk temurin-21-jdk
